d678deff88fd66feadec1f5f15ba9db6bd81b934
Earl Hans Genoso <earlhansg@gmail.com>
2023-05-12 23:14:09 +0800

    T1-The content of the header device has been augmented, specifically focusing on numbers for equations and the total solution.

--- src/components/calculator/DeviceHeader.js
+++ src/components/calculator/DeviceHeader.js
@@ -2,5 +2,20 @@ import React from "react";
 import style from "./device.module.css";
 
 export const DeviceHeader = () => {
-  return <div className={style.header}></div>;
+  return (
+    <div className={style.header}>
+      <div className={style.numberContainer}>
+        <div className={style.inputs}>
+          <span>12</span>
+          <span>-</span>
+          <span>4</span>
+          <span>+</span>
+          <span>4</span>
+        </div>
+        <div className={style.totals}>
+          <span>78.58</span>
+        </div>
+      </div>
+    </div>
+  );
 };
