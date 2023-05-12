97c907387df2bb75710f493d258422c4909d6fc4
Earl Hans Genoso <earlhansg@gmail.com>
2023-05-13 01:09:58 +0800

    T1-The design of the calculator body has been finalized, incorporating primary and secondary classes.

--- src/components/calculator/DeviceBody.js
+++ src/components/calculator/DeviceBody.js
@@ -12,6 +12,12 @@ export const DeviceBody = () => {
         <span>7</span>
         <span>8</span>
         <span>9</span>
+        <span>4</span>
+        <span>5</span>
+        <span>6</span>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
       </div>
       <div className={style.secondaryContent}>
         <span>/</span>
