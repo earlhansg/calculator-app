ffd80eac268e921085e9d408d7b022d06844a5eb
Earl Hans Genoso <earlhansg@gmail.com>
2023-05-13 00:21:55 +0800

    T1-The initial design for the calculator body encompasses numbers and operators.

--- src/components/calculator/DeviceBody.js
+++ src/components/calculator/DeviceBody.js
@@ -2,5 +2,25 @@ import React from "react";
 import style from './device.module.css'
 
 export const DeviceBody = () => {
-  return <div className={style.body}>DeviceBody</div>;
+  return <div className={style.body}>
+
+    <div className={style.bodyContent}>
+      <div className={style.primaryContent}>
+        <span>AC</span>
+        <span>+/-</span>
+        <span>%</span>
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+      </div>
+      <div className={style.secondaryContent}>
+        <span>/</span>
+        <span>*</span>
+        <span>-</span>
+        <span>+</span>
+        <span>=</span>
+      </div>
+    </div>
+
+  </div>;
 };
